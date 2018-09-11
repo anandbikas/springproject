008801bfbdc64fedb0df0a7b9ea8d3b52d9348e0
Bikas Anand <anand.iitr.mca@gmail.com>
2018-09-11 14:14:23 +0530

    DocController and excel document operation

---
+++ src/main/java/com/anand/springproject/service/DocService.java
@@ -0,0 +1,44 @@
+package com.anand.springproject.service;
+
+import com.anand.springproject.domains.Student;
+import com.anand.springproject.utils.excel.ExcelDocument;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ */
+@Component
+public class DocService {
+
+    final static String STUDENTS = "students";
+
+    @Autowired
+    ExcelDocument excelDocument;
+
+    /**
+     *
+     * @param rollNumber
+     * @return
+     */
+    public Student getDocument(String namespace, int rollNumber) throws Exception{
+
+        switch (namespace.toLowerCase()){
+            case STUDENTS:
+                Student student = new Student();
+
+                student.setRollNumber(Integer.valueOf(excelDocument.getFieldValue(Student.ROLL_NUMBER, rollNumber)));
+                student.setFirstName(excelDocument.getFieldValue(Student.FIRST_NAME, rollNumber));
+                student.setLastName(excelDocument.getFieldValue(Student.LAST_NAME, rollNumber));
+                student.setContact(excelDocument.getFieldValue(Student.CONTACT, rollNumber));
+                student.setAddress(excelDocument.getFieldValue(Student.ADDRESS, rollNumber));
+                student.setCity(excelDocument.getFieldValue(Student.CITY, rollNumber));
+
+                return student;
+            default:
+                throw new Exception("Doc " + namespace + " not supported");
+        }
+
+    }
+
+}
---
+++ src/main/java/com/anand/springproject/utils/excel/ExcelDocument.java
@@ -0,0 +1,94 @@
+package com.anand.springproject.utils.excel;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.slf4j.ext.XLogger;
+import org.slf4j.ext.XLoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.util.HashMap;
+
+@Component
+public class ExcelDocument {
+
+    private static final XLogger logger = XLoggerFactory.getXLogger(ExcelDocument.class);
+
+    @Value("${excel.file.name}")
+    private String excelFileName;
+
+    private static final DataFormatter dataFormatter = new DataFormatter();
+
+    private HSSFSheet sheet;
+    private int rows = 0;
+
+    HashMap<String, Integer> fieldCellMap = new HashMap<>();
+
+    /**
+     *
+     */
+    @PostConstruct
+    public void readFile() {
+        try{
+            POIFSFileSystem fs = new POIFSFileSystem(new FileInputStream(excelFileName));
+
+            HSSFWorkbook wb = new HSSFWorkbook(fs);
+            sheet = wb.getSheetAt(0);
+            rows = sheet.getPhysicalNumberOfRows();
+            
+        } catch (Exception ex){
+            logger.debug("Failed to initialize excel document");
+            rows=0;
+            return;
+        }
+
+        //Populate FieldCellMap
+        HSSFRow headerRow = sheet.getRow(0);
+        for(int cell = 0; cell < headerRow.getPhysicalNumberOfCells(); cell++){
+            HSSFCell hssfCell = headerRow.getCell(cell);
+            final String field = dataFormatter.formatCellValue(hssfCell);
+
+            if(StringUtils.isEmpty(field)){
+                continue;
+            }
+            fieldCellMap.put(field, cell);
+        }
+        logger.info("ExcelDocument initialized...");
+    }
+
+    /**
+     *
+     * @param field
+     * @param row
+     * @return
+     */
+    public String getFieldValue(final String field, final int row){
+
+        if(row>rows-1){
+            throw new IndexOutOfBoundsException("Sheet row not found: " + row);
+        }
+
+        if(!fieldCellMap.containsKey(field)){
+            throw new IllegalArgumentException("Field not found: " + field);
+        }
+
+        HSSFCell hssfCell = sheet.getRow(row).getCell(fieldCellMap.get(field));
+        String value = dataFormatter.formatCellValue(hssfCell);
+        return value;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getRows() {
+        return rows;
+    }
+}
