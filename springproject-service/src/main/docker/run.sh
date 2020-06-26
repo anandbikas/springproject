14a6a9d20bb8ec427649a3843c4e3c1d9e0fb41b
Bikas Anand <anand.iitr.mca@gmail.com>
2020-06-27 00:27:09 +0530

    Add JMX configuration for SpringBoot App JVM profiling.

--- springproject-service/src/main/docker/run.sh
+++ springproject-service/src/main/docker/run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-APP_HOME="-DAPP_HOME=$APP_HOME"
+APP_HOME_VAR="-DAPP_HOME=$APP_HOME"
 
 [[ -z "$DEBUG_PORT" ]] && DEBUG="" || DEBUG="-agentlib:jdwp=transport=dt_socket,address=$DEBUG_PORT,server=y,suspend=n"
+[[ -z "$JMX_PORT" ]] && JMX_OPTS="" || JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=$JMX_PORT -Dcom.sun.management.jmxremote.rmi.port=$JMX_PORT -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=localhost -Dcom.sun.management.jmxremote.local.only=false"
 
-JAVA_ARGS=" $APP_HOME $JAVA_OPTS $DEBUG"
+JVM_OPTS="-Xms512M -Xmx1024M"
+JAVA_ARGS=" $APP_HOME_VAR $JAVA_OPTS $JVM_OPTS $JMX_OPTS $DEBUG"
 
 exec java -server $JAVA_ARGS -jar $SERVICE_JAR --spring.config.location=classpath:application.properties,$APP_HOME/conf/application-override.properties
