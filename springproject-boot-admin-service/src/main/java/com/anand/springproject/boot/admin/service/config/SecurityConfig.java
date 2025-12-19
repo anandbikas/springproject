1d5523c54452b939750921ec3fa08dfce280ff0a
Bikas Anand <anand.iitr.mca@gmail.com>
2025-12-19 23:45:51 +0530

    Spring Security 6

--- springproject-boot-admin-service/src/main/java/com/anand/springproject/boot/admin/service/config/SecurityConfig.java
+++ springproject-boot-admin-service/src/main/java/com/anand/springproject/boot/admin/service/config/SecurityConfig.java
@@ -2,11 +2,17 @@ package com.anand.springproject.boot.admin.service.config;
 
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -14,53 +20,59 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import java.util.UUID;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
     private final AdminServerProperties adminServer;
     private final SecurityProperties security;
+    private final String contextPath;
 
     public SecurityConfig(AdminServerProperties adminServer, SecurityProperties security) {
         this.adminServer = adminServer;
         this.security = security;
+        this.contextPath = adminServer.getContextPath();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-            .authorizeRequests()
-                .antMatchers(this.adminServer.getContextPath() + "/assets/**").permitAll()
-                .antMatchers(this.adminServer.getContextPath() + "/login").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage(this.adminServer.getContextPath() + "/login")
-                .successHandler(successHandler())
-                .and()
-            .logout()
-                .logoutUrl(this.adminServer.getContextPath() + "/logout")
-                .and()
-            .httpBasic()
-                .and()
-            .csrf()
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                .ignoringRequestMatchers(
-                  new AntPathRequestMatcher(this.adminServer.getContextPath() + "/instances", HttpMethod.POST.toString()),
-                  new AntPathRequestMatcher(this.adminServer.getContextPath() + "/instances/*", HttpMethod.DELETE.toString()),
-                  new AntPathRequestMatcher(this.adminServer.getContextPath() + "/actuator/**"))
-                .and()
-            .rememberMe()
-                .key(UUID.randomUUID().toString())
-                .tokenValiditySeconds(1209600);
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(contextPath + "/assets/**").permitAll()
+                        .requestMatchers(contextPath + "/login").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(fl -> fl
+                        .loginPage(contextPath + "/login")
+                        .successHandler(successHandler()))
+                .logout(lc -> lc
+                        .logoutUrl(contextPath + "/logout"))
+                .httpBasic(Customizer.withDefaults())
+                .csrf(cc -> cc
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .ignoringRequestMatchers(
+                                AntPathRequestMatcher.antMatcher(HttpMethod.POST, contextPath + "/instances"),
+                                AntPathRequestMatcher.antMatcher(HttpMethod.DELETE, contextPath + "/instances/*"),
+                                AntPathRequestMatcher.antMatcher(contextPath + "/actuator/**")))
+                .rememberMe(rmc -> rmc
+                        .key(UUID.randomUUID().toString())
+                        .tokenValiditySeconds(1209600));
 
+        return http.build();
     }
 
     // Required to provide UserDetailsService for "remember functionality"
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser(security.getUser().getName())
-                .password("{noop}" + security.getUser().getPassword())
-                .roles("USER");
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails userDetails = User
+                .withUsername(security.getUser().getName())
+                .password(passwordEncoder().encode(security.getUser().getPassword()))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     private SavedRequestAwareAuthenticationSuccessHandler successHandler(){
--- springproject-service/src/main/java/com/anand/springproject/service/security/config/SecurityConfig.java
+++ springproject-service/src/main/java/com/anand/springproject/service/security/config/SecurityConfig.java
@@ -4,25 +4,29 @@ import com.anand.springproject.service.security.authentication.SSLAuthentication
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.authentication.AuthenticationManagerBeanDefinitionParser;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 
-
+/**
+ * <a href="https://www.baeldung.com/spring-deprecated-websecurityconfigureradapter">...</a>
+ */
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     @Value("${server.ssl.client-auth:none}")
     private String serverSslClientAuth;
@@ -30,18 +34,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Value("${server.ssl.enabled:false}")
     private boolean serverSslEnabled;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
         http
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-                .exceptionHandling()
-                .authenticationEntryPoint(serverAuthenticationEntryPoint())
-        .and()
-                .csrf().disable()
-                .formLogin()
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(httpSecurityExceptionHandlingConfigurer ->
+                        httpSecurityExceptionHandlingConfigurer.authenticationEntryPoint(serverAuthenticationEntryPoint()))
+                .csrf(Customizer.withDefaults())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authenticationProvider(authenticationProvider())
         ;
 
         if(serverSslEnabled && serverSslClientAuth.equalsIgnoreCase("want")){
@@ -60,6 +62,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                    .subjectPrincipalRegex("CN=(.*?)(?:,|$)")
 //                    .userDetailsService(userDetailsService());
         }
+        return http.build();
     }
 
     /**
@@ -81,20 +84,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         };
     }
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring()
-                .antMatchers("/error")
-                .antMatchers("/status")
-                .antMatchers("/assets/**")
-                .antMatchers("/actuator/**")
-                .antMatchers("/webjars/**");
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/error")
+                .requestMatchers("/status")
+                .requestMatchers("/assets/**")
+                .requestMatchers("/actuator/**")
+                .requestMatchers("/webjars/**");
     }
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(
-                new AuthenticationManagerBeanDefinitionParser.NullAuthenticationProvider());
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        return new AuthenticationManagerBeanDefinitionParser.NullAuthenticationProvider();
     }
 
     @Bean
